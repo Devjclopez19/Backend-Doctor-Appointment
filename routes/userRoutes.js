aebb609392913fbed385e5b221730ed9c9fa640f
Dev. JCLopez <dev.jclopez@gmail.com>
2023-06-29 17:01:25 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -24,7 +24,7 @@ router.post("/login", loginController);
 router.post("/register", registerController);
 
 // Auth || POST
-router.post("/getUserData", authMiddleware, authController);
+router.get("/getUserData", authMiddleware, authController);
 
 // Apply Doctor || POST
 router.post("/apply-doctor", authMiddleware, applyDoctorController);
